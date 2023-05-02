e8bebe9d469cf6c6cc9fe397719d2c8d2f4465b2
humanity1111 <xoliqovmuzaffar240@gmail.com>
2023-05-02 20:30:43 +0500

    tailwind-task

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [`./index.html`],
+  theme: {
+    extend: {},
+    container: {
+      center: `auto`
+    }
+    // body: {
+    //   container: `uppercase`
+    // }
+  },
+  plugins: [],
+}
